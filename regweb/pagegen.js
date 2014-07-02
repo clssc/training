178d4bbd898617f43a51791c8cc169ad6e70ccd4
arthurhsu <artchsu@gmail.com>
2014-07-02 01:04:20 -0700

    Stashing work

---
+++ regweb/pagegen.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview Web page generator for Westside Chinese School registration.
+ * This code is supposed to be run using node.js. Usage:
+ *   node pagegen.js <template> <CSS> <JS> <language>
+ * @author Arthur Hsu (arthurhsu@westsidechineseschool.org)
+ */
+var path = require('path');
+var file = require('file');
+

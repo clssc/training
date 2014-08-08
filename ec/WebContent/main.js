000f69c4d062e7968636da61f1e6af81b66cc442
charlesyy77 <charlietheunicorpse@gmail.com>
2014-08-08 16:16:18 -0700

    Create main.js

---
+++ ec/WebContent/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	
+	$("#swag" )
+	.click(function() {
+		var FirstName = $("#firstname").val();
+		var LastName = $("#lastname").val();
+		var ChineseSchoolID = $("#ChineseSchoolID").val();	
+		var Classes = $("#fancypantslist").val();
+		console.log(FirstName);
+		console.log(LastName);
+		console.log(ChineseSchoolID);				
+		console.log(Classes);
+		
+	});	
+
+	
+	
+});

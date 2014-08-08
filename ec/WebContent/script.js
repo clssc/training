6270487a6b465d504cf9b02e193c4ab445d05118
1walterh <weitih@gmail.com>
2014-08-08 13:14:01 -0700

    Homework
    done. no bonus

---
+++ ec/WebContent/script.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+	//console.clear();
+	$("#button1" ).click(function() {
+		var FirstName = $("#first_name").val();
+		var LastName = $("#last_name").val();
+		var ChineseSchoolID = $("#chinese_school_ID").val();	
+		var SelectedClass = $("#drop_down_list").val();
+		console.log(FirstName);
+		console.log(LastName);
+		console.log(ChineseSchoolID);				
+		console.log(SelectedClass);
+		//$("p").append("<p>" + FirstName + "</p>")
+	});	
+
+	
+	
+})

3e0653e417603a1b54e28021445e4ade0409bfbc
arthurhsu <artchsu@gmail.com>
2014-08-08 23:41:10 -0700

    Add communication code to sample app.

--- sample/main2.js
+++ sample/main2.js
@@ -4,8 +4,28 @@ $('#text').addClass('blue');
 
 $('#text').after('<p class="red">Dynamically created text in red</p>');
 
+function submitData() {
+  var data = {
+    'name': 'sample1',
+    'phone': '310-310-3310'
+  };
+
+  var messageBody = JSON.stringify(data);
+  console.log('sending', messageBody);
+  $.ajax({
+    type: 'POST',
+    url: 'https://script.google.com/macros/s/AKfycbzuYsOLiP8HS5D-X2PmGYVnQ9-84kmPmstsQPmvKTS3HO3gfGx1/exec',
+    data: messageBody,
+    dataType: 'text'
+  }).done(function(ok) {
+    alert('server said ' + ok);
+  }).fail(function(err) {
+    alert('server rejected ' + err);
+  });
+}
+
 $('#fire')
   .attr('value', 'Click Me!')
   .click(function() {
-    alert('Thank you for clicking me!');
+    submitData();
   });

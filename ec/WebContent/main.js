e21997d28c96b42ce41c3dea968ff70ffe49d0fa
charlesyy77 <charlietheunicorpse@gmail.com>
2014-08-23 14:00:18 -0700

    Update main.js

--- ec/WebContent/main.js
+++ ec/WebContent/main.js
@@ -13,6 +13,18 @@ $(document).ready(function() {
 		
 	});	
 
-	
+	var messageBody = JSON.stringify(data);
+  console.log('sending', messageBody);
+  $.ajax({
+    type: 'POST',
+    url: 'https://script.google.com/macros/d/1yEeKgEeniuOaODEmsMepdVEUgE_nZTC_FIyncQL9HuMUhDeZ3aqYke3t/edit?template=default&folder=0AIpx_eXx1bMcUk9PVA&usp=drive_web',
+    data: messageBody,
+    dataType: 'text'
+  }).done(function(ok) {
+    alert('server said ' + ok);
+  }).fail(function(err) {
+    alert('server rejected ' + err);
+  });
+}
 	
 });
